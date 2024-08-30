909d13992a48d62ede244d0006ca108b0feefc41
SumitJ2002 <sumitjaiswarsj420@gmail.com>
2024-08-30 20:51:53 +0530

    secon commit

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
-const express = require('express');
+const express = require("express");
 const cookieParser = require("cookie-parser");
-const cors = require('cors');
-require('dotenv').config();
-const connection = require('./db')
+const cors = require("cors");
+require("dotenv").config();
+const connection = require("./db");
 connection();
 const app = express();
 app.use(express.json());
-app.use(cors({
-    origin: 'https://quickadmitplus.vercel.app',
+app.use(
+  cors({
+    origin: "https://quickadmitplus-frontend.vercel.app/",
     credentials: true,
-}));
+  })
+);
 app.use((req, res, next) => {
-    res.header('Access-Control-Expose-Headers', 'jwtoken');
-    res.setHeader('Access-Control-Allow-Origin', 'https://quickadmitplus.vercel.app')
-    next();
+  res.header("Access-Control-Expose-Headers", "jwtoken");
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://quickadmitplus-frontend.vercel.app/"
+  );
+  next();
 });
 app.use(cookieParser());
-app.use(require('./routes/auth'));
+app.use(require("./routes/auth"));
 const port = 8000;
 app.listen(port, () => {
-    console.log(`Server is Running on ${port} port`);
-})
+  console.log(`Server is Running on ${port} port`);
+});
--- middleware/hospitalmiddleware.js
+++ middleware/hospitalmiddleware.js
@@ -1,28 +1,33 @@
-const Hospital = require('../models/HospitalSchema');
-const jwt = require('jsonwebtoken');
+const Hospital = require("../models/HospitalSchema");
+const jwt = require("jsonwebtoken");
 const hospitalmiddleware = async (req, res, next) => {
-    try {
-        res.setHeader('Access-Control-Allow-Origin', 'https://quickadmitplus.vercel.app');
-        const token = req.headers['jwtoken'];
-        if (!token) {
-            return res.status(401).send('Unauthorized: No Token Provided');
-        }
-        const verifyToken = jwt.verify(token,process.env.SECRET_KEY);
-        let rootUser = await Hospital.findOne({_id:verifyToken._id,"tokens.token":token});
-        if(!rootUser) {
-            throw new Error('Hospital Not Found')
-        }
-        req.token = token;
-        req.rootUser = rootUser;
-        req.hospitalid = rootUser.hospitalid;
-        req.email = rootUser.email;
-        req.image = rootUser.image;
-        req.contentType = rootUser.contentType;
-        next();
+  try {
+    res.setHeader(
+      "Access-Control-Allow-Origin",
+      "https://quickadmitplus-frontend.vercel.app/"
+    );
+    const token = req.headers["jwtoken"];
+    if (!token) {
+      return res.status(401).send("Unauthorized: No Token Provided");
     }
-    catch(err) {
-        res.status(401).send('Unauthorized : No Token Provided');
-        console.log(err);
+    const verifyToken = jwt.verify(token, process.env.SECRET_KEY);
+    let rootUser = await Hospital.findOne({
+      _id: verifyToken._id,
+      "tokens.token": token,
+    });
+    if (!rootUser) {
+      throw new Error("Hospital Not Found");
     }
-}
+    req.token = token;
+    req.rootUser = rootUser;
+    req.hospitalid = rootUser.hospitalid;
+    req.email = rootUser.email;
+    req.image = rootUser.image;
+    req.contentType = rootUser.contentType;
+    next();
+  } catch (err) {
+    res.status(401).send("Unauthorized : No Token Provided");
+    console.log(err);
+  }
+};
 module.exports = hospitalmiddleware;
